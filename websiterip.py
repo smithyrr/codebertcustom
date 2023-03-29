9981c086d31a35cff9b0fda7a473abcefdb45475
smithyrr <smithyyr@gmail.com>
2023-03-29 19:35:01 +0000

    added arma3 commands

--- websiterip.py
+++ websiterip.py
@@ -1,55 +1,32 @@
 import requests
-import os
 from bs4 import BeautifulSoup
+import json
 
-# Define the base URLs of the websites
-bohemia_url = "https://community.bistudio.com"
-other_url = "https://dayzexplorer.zeroy.com"
+url = "https://community.bistudio.com/wiki/Category:Arma_3:_Scripting_Commands"
+base_url = "https://community.bistudio.com"
+response = requests.get(url)
 
-# Define a function to download a code example and save it to a file
-def download_code_example(url, subdir, example_num):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    # Find the HTML "code" tag that contains the example code
-    code_tag = soup.find("code")
-    if code_tag is not None:
-        # Extract the example code and save it to a file
-        code_example = code_tag.get_text()
-        filename = f"{subdir}_example{example_num}.sqf"
-        with open(filename, "w") as f:
-            f.write(code_example)
+soup = BeautifulSoup(response.text, "html.parser")
+commands_list = soup.find_all("div", class_="mw-category-group")
 
-# Get the HTML content of the "Category:Arma_3:_Scripting_Commands" page on the Bohemia Interactive Community Wiki
-response = requests.get(bohemia_url + "/wiki/Category:Arma_3:_Scripting_Commands")
-soup = BeautifulSoup(response.content, "html.parser")
+commands_data = []
 
-# Find the HTML "div" tag that contains the list of sub-directories
-subdir_div = soup.find("div", {"id": "mw-pages"})
+for group in commands_list:
+    commands = group.find_all("a")
+    for command in commands:
+        command_url = base_url + command["href"]
+        command_response = requests.get(command_url)
+        command_soup = BeautifulSoup(command_response.text, "html.parser")
+        
+        command_name = command.text.strip()
+        command_description = command_soup.find("div", class_="description").get_text(strip=True) if command_soup.find("div", class_="description") else "No description available."
+        command_example = command_soup.find("pre", class_="code-snippet-example").get_text(strip=True) if command_soup.find("pre", class_="code-snippet-example") else "No example available."
+        
+        commands_data.append({
+            "name": command_name,
+            "description": command_description,
+            "example": command_example
+        })
 
-# Find all HTML "a" tags within the "div" tag and extract their contents and URLs
-subdir_links = subdir_div.find_all("a")
-for link in subdir_links:
-    subdir_name = link.get_text()
-    subdir_url = bohemia_url + link["href"]
-    subdir_response = requests.get(subdir_url)
-    subdir_soup = BeautifulSoup(subdir_response.content, "html.parser")
-    # Find all HTML "a" tags within the sub-directory and extract their contents and URLs
-    example_links = subdir_soup.find_all("a", {"class": "mw-redirect"})
-    for i, link in enumerate(example_links):
-        example_name = link.get_text()
-        example_url = bohemia_url + link["href"]
-        # Download and save the code example to a file
-        download_code_example(example_url, subdir_name, i)
-
-# Get the HTML content of the DayZ Explorer website
-response = requests.get(other_url)
-soup = BeautifulSoup(response.content, "html.parser")
-
-# Find all HTML "code" tags within the website and extract their contents
-code_tags = soup.find_all("code")
-for i, tag in enumerate(code_tags):
-    code_example = tag.get_text()
-    # Save the code example to a file
-    filename = f"dayz_example{i}.txt"
-    with open(filename, "w") as f:
-        f.write(code_example)
+with open("arma3_commands.json", "w") as file:
+    json.dump(commands_data, file, indent=4)
