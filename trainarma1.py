b710810e49ebf56bf6d5470a4c215f743d73d2a7
smithyrr <smithyyr@gmail.com>
2023-03-31 16:21:27 +0000

    modified few files

--- trainarma1.py
+++ trainarma1.py
@@ -1,93 +1,102 @@
-import json
-import os
+import torch
+from torch.utils.data import DataLoader
+from transformers import (
+    AutoTokenizer,
+    AutoModelForMaskedLM,
+    DataCollatorForLanguageModeling,
+    Trainer,
+    TrainingArguments,
+)
 import wandb
-from transformers import AutoTokenizer, AutoModelForMaskedLM, LineByLineTextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 
-# Set up Weights and Biases (wandb) for experiment tracking
-wandb.login()
+# Initialize a W&B run
 wandb.init(project="arma3_codebert", name="training_run")
 
-# Load JSON file
-with open("/home/cognitron/codebert/arma3_commands_with_descriptions.json") as f:
-    data = json.load(f)
-
-# Extract code snippets from JSON file
-code_snippets = []
-for item in data:
-    code_snippets.append(item["description"])
-
-# Tokenize code snippets using CodeBERT tokenizer
+# Load the tokenizer and the model
 tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
-tokenized_code = tokenizer.batch_encode_plus(
-    code_snippets,
-    add_special_tokens=True,
-    padding="max_length",
-    max_length=512,
-    return_tensors="pt"
-)
-
-# Create training dataset from tokenized code
-dataset = LineByLineTextDataset(
-    tokenizer=tokenizer,
-    file_path=None,
-    batch_size=16,
-    encoding="utf-8",
-    limit_length=2048,
-    max_length=512,
-    lines=tokenized_code["input_ids"],
-)
-
-# Set up data collator for language modeling
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer,
-    mlm=True,
-    mlm_probability=0.15
-)
-
-# Load CodeBERT model
 model = AutoModelForMaskedLM.from_pretrained("microsoft/codebert-base")
 
-# Set up training arguments
+# Load your custom dataset
+code_names_file = "/codebertcustom/arma3/data/ready/code_names.txt"
+descriptions_file = "/codebertcustom/arma3/data/ready/descriptions.txt"
+
+with open(code_names_file, "r") as f:
+    code_names = f.readlines()
+
+with open(descriptions_file, "r") as f:
+    descriptions = f.readlines()
+
+# Tokenize the data
+input_texts = [code.strip() for code in code_names]
+target_texts = [desc.strip() for desc in descriptions]
+
+inputs = tokenizer(input_texts, return_tensors="pt", padding=True, truncation=True)
+targets = tokenizer(target_texts, return_tensors="pt", padding=True, truncation=True)
+
+# Create a dataset
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, inputs, targets):
+        self.inputs = inputs
+        self.targets = targets
+
+    def __len__(self):
+        return len(self.inputs["input_ids"])
+
+    def __getitem__(self, idx):
+        input_ids = self.inputs["input_ids"][idx]
+        attention_mask = self.inputs["attention_mask"][idx]
+        target_ids = self.targets["input_ids"][idx]
+        target_attention_mask = self.targets["attention_mask"][idx]
+
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": target_ids,
+            "labels_attention_mask": target_attention_mask,
+        }
+
+dataset = CustomDataset(inputs, targets)
+
+# Create a DataLoader
+data_loader = DataLoader(dataset, batch_size=2, shuffle=True)
+
+# Set training arguments
 training_args = TrainingArguments(
-    output_dir="/home/cognitron/codebert/training",
+    output_dir="./results",
     overwrite_output_dir=True,
-    num_train_epochs=10,
-    per_device_train_batch_size=16,
+    num_train_epochs=3,
+    per_device_train_batch_size=2,
+    save_steps=5000,
     save_total_limit=2,
-    prediction_loss_only=True,
+    logging_dir="./logs",
     logging_steps=500,
-    save_steps=1000,
-    eval_steps=1000,
-    learning_rate=5e-5,
-    warmup_steps=500
 )
 
-# Set up wandb logging for experiment tracking
-training_args.report_to = "wandb"
-training_args.log_train_metrics = True
-training_args.log_eval_metrics = True
-training_args.evaluate_during_training = True
+# Define a data collator
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=True, mlm_probability=0.15
+)
 
-# Set up wandb logging for model checkpointing
-training_args.save_strategy = "steps"
-training_args.save_steps = 1000
-training_args.save_total_limit = 2
-training_args.save_steps = 500
-
-# Set up wandb logging for alerting and notifications
-training_args.enable_app_mode = True
-training_args.notify_email = "smithyyr@gmail.com"
-training_args.early_stopping_patience = 5
-training_args.early_stopping_threshold = 0.01
-
-# Set up Trainer object and start training
+# Create a Trainer
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=dataset,
     data_collator=data_collator,
+    callbacks=[
+        wandb.tensorboard.WandbCallback(log_model=True, log_preds=True),
+        wandb.Callback(log="all")
+    ]
 )
+
+# Train the model
 trainer.train()
 
-# Finish wandb run
-wandb.finish()
+# Log hyperparameters
+wandb.config.update(training_args)
+
+# Send alert notification when training completes
+wandb.alert(title="Training Complete", text="Your model training has completed!", level=wandb.AlertLevel.SUCCESS)
+
+# Log custom metadata
+wandb.log({"dataset_size": len(dataset), "batch_size": training_args.per_device_train_batch_size})
